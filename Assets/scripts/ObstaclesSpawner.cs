328c4af34751dd4422b17690cd8372f3fed39a66
BartekGG <bartosz.chajdas@gmail.com>
2019-11-07 12:10:16 +0100

    Flappy 1.0

---
+++ Assets/scripts/ObstaclesSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesSpawner : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer = 0;
+    public GameObject obstacles;
+    public float height;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObstacles = Instantiate(obstacles);
+        newObstacles.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject newObstacles = Instantiate(obstacles);
+            newObstacles.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newObstacles, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
